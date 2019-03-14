73af41d7200016146c9b2194f8fa344bd8454a87
cherule <theswush1029@gmail.com>
2019-03-14 20:55:33 +0100

    Fix crash on startup

--- Riesenneger bei Nieselregen/main.js
+++ Riesenneger bei Nieselregen/main.js
@@ -20,18 +20,3 @@ app.on('ready', function(){
 });
 
 // if(process.env.NODE_ENV != 'production')
-
-p2pChannel.initChannel();
-
-const WindowPeerConnection = require("electron-peer-connection").WindowPeerConnection;
-let windowA = new WindowPeerConnection('windowA');
-windowA.attachStream(stream);
-windowA.sendStream('windowB');
-
-const WindowPeerConnection = require("electron-peer-connection").WindowPeerConnection;
-let windowB = new WindowPeerConnection('windowB');
-windowB.onReceivedStream(function (stream) {    
-    video.srcObject = stream;
-});
-p2pChannel.addClient({window: windowA, name: 'windowA'});
-p2pChannel.addClient({window: windowB, name: 'windowB'});
