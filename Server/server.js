9d76990739e84ae1ecdd1dc3f0f47afa3cf2e1e9
cherule <theswush1029@gmail.com>
2019-04-06 19:30:19 +0200

    Implemented alarm handler

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var socket  = require('socket.io');
 
 const PORT = 9000; 
+var currentAlarmLevel;
 
 //App
 var app = express();
@@ -32,8 +33,35 @@ io.on('connection', (socket) => {
 
     });
 
-
     socket.on('disconnect', (socket)  => {
         console.log('socket disconnected:', socket.id);
     });
+
+    socket.on('alarm', function(data){
+
+        if(data.type == 'trigger')
+        {
+            // If the same alarm is requested twice in a row, it will be cancelled
+            if(currentAlarmLevel == data.level){
+                //Send a custom package to the client, that will stop the current playing alarm(sound) 
+                io.sockets.emit('alarm', {
+                    type: 'alarm_stop',
+                    level: data.level
+                });
+                console.log('Stopped: ' + data.level);         
+                currentAlarmLevel = 0;        
+            }
+            // Normal alarm
+            else{ 
+                io.sockets.emit('alarm', data);
+                console.log(data);
+                currentAlarmLevel = data.level;
+            } 
+        }
+        // Abort (alarm 0) requested
+        else{ 
+            io.sockets.emit('alarm', data);
+            console.log(data);
+        }
+    }); 
 });
