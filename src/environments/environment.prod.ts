46ae3882f999a43d74d67c1e0ab34d2164180a8a
Wooops <patrick.waldhauser@outlook.at>
2019-11-18 19:32:21 +0100

    First running Version of WebFrontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:9000/'
+  apiUrl: 'http://localhost:9001/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9000/'
+  apiUrl: 'http://localhost:9001/'
 
   // Testserver
   //apiUrl: 'http://25.66.153.178:9000/'
